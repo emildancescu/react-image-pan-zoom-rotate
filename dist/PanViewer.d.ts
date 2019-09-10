ebfb3de3e207c45f95adce0d9b1b62bda51d22e3
Emil <dancescu_emilian@yahoo.com>
2019-09-10 17:08:41 +0300

    quick hack: upload dist

---
+++ dist/PanViewer.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+declare class PanViewer extends React.Component<{
+    image: string;
+    alt?: string;
+}> {
+    state: {
+        dx: number;
+        dy: number;
+        zoom: number;
+        rotation: number;
+    };
+    renderPanZoomControls: () => JSX.Element;
+    render(): JSX.Element[];
+    zoomIn: () => void;
+    zoomOut: () => void;
+    rotateLeft: () => void;
+    onPan: (dx: number, dy: number) => void;
+}
+export default PanViewer;
